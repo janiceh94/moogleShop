b49e8397de4717407b803381c946886b223491a4
janiceh94 <haesung94@gmail.com>
2022-07-12 21:52:41 -0700

    created components folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Landing from './Landing';
+import Landing from './components/Landing';
 
 ReactDOM.render(
   <React.StrictMode>
